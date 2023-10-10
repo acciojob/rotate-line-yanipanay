c55018e58128eb1f54f31a8352172be21b336f82
yani pranay <33549205+yanipanay@users.noreply.github.com>
2023-10-11 00:53:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const line = document.getElementById("line");
+
+setInterval(rotate,20);
+let deg = 0;
+function rotate() {
+	deg+=2;
+	line.style.transform = `rotate(${deg}deg)`
+}
